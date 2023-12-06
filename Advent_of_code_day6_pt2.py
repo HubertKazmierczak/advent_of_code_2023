daccd39c147a318d7abcc120745075e2d97214dd
HubertKazmierczak <141769245+HubertKazmierczak@users.noreply.github.com>
2023-12-06 18:38:13 +0100

    Add files via upload

---
+++ Advent_of_code_day6_pt2.py
@@ -0,0 +1,34 @@
+def clear_data(data):
+    new_data = [""]
+    while True:
+        if "" in data:
+            data.remove("")
+        else:
+            break
+    for i in data:
+        new_data[0] = new_data[0] + i
+    new_data = list(map(int,new_data))
+    return new_data
+data1 = clear_data(open("input.txt").read().split("\n")[0].split(":")[1].split(" "))
+data2 = clear_data(open("input.txt").read().split("\n")[1].split(":")[1].split(" "))
+
+index_to_distance = {}
+for i in range(len(data1)):
+    index_to_distance[i]= data2[i]
+def get_distance(time,total):
+    return (total-time)*time
+ways = 1
+for i in range(len(data1)):
+    wins = 0
+    z = data1[i]
+    for y in range(z + 1):
+        if get_distance(y,z) > index_to_distance[i]:
+            wins +=1
+    ways = ways * wins
+
+print(ways)
+
+
+
+
+
